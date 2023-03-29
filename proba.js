4c292909115920dd52ef25852f57d4330bd973f8
Lovro Luka Matan <lmatan@unipu.hr>
2023-03-29 10:42:15 +0200

    Update proba.js

--- proba.js
+++ proba.js
@@ -1,9 +1,31 @@
+
+const books = [
+    { title: "The grat gatsby", author: "F. Scott Fitzgeral", publicationYear: 1925 },
+    { title: "TO kill mockingbird", author: "Harper Lee", publicationYear: 1960 }
+]
+
+for (let book of books) {
+    book["sales"] = Math.floor(Math.random() * 100);
+}
+
+const booksWithSales = books.map(book => {
+    return {
+        title: book.title,
+        author: book.author,
+        publicationYear: book.publicationYear,
+        sales: Math.floor(Math.random() * 100)
+    }
+})
+//console.log(booksWithSales);
+//console.log(books);
+/*
 let students = [
     { name: "john", age: 21, gender: "male", grade: 85 },
     { name: "mary", age: 19, gender: "female", grade: 92 },
     { name: "DAVID", age: 25, gender: "male", grade: 78 },
     { name: "ana", age: 22, gender: "female", grade: 89 }
 ]
+//console.log(students);
 
 function findTopStudent() {
     let topGrade = 0;
@@ -18,13 +40,14 @@ function findTopStudent() {
 
     return topFemaleStudent;
 }
-console.log(findTopStudent());
-
+//console.log(findTopStudent());
+/*
 //3.
 const books = [
     { title: "the great gatsby", author: "f. scott fitgerald", year: 1925 },
     { title: "to kill a mocking bird", author: "harp lee", year: 1960 }
 ]
+//console.log(books);
 
 for (let book of books) {
     book["sales"] = Math.floor(Math.random() * 100);
@@ -39,8 +62,8 @@ const booksWithSales = book.map(book => {
     }
 })
 
-console.log(books);
-
+//console.log(booksWithSales);
+*/
 //4
 
 let bookTitle = books.filter((book) => {
